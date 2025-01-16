a87da7b55e0b078103892e838a2befd1ae3e5ddd
Lucas Lima <lucasvinilima15@gmail.com>
2025-01-15 21:28:30 -0300

    feat: adicionando funcionalidade de filtrar trabsações

--- dashboard/src/modules/dashboard/components/Transactions.tsx
+++ dashboard/src/modules/dashboard/components/Transactions.tsx
@@ -8,30 +8,32 @@ import TransactionModal from "./TransactionModal";
 
 export default function Transactions() {
   const TRANSACTION_TYPES = {
-    all: 'Todos',
-    deposit: 'Depósito',
-    transfer: 'Transferência',
-    withdraw: 'Saque',
-    payment: 'Pagamento',
+    all: "Todos",
+    deposit: "Depósito",
+    transfer: "Transferência",
+    withdraw: "Saque",
+    payment: "Pagamento",
   };
 
   const [loading, setLoading] = useState(false);
-  const [transactions, setTransactions] = useRecoilState(transactionsState)
-  const [selectedFilter, setSelectedFilter] = useState('all');
-  const [selectedTransaction, setSelectedTransaction] = useState(null);
+  const [transactions, setTransactions] = useRecoilState(transactionsState);
+  const [selectedFilter, setSelectedFilter] = useState("all");
+  const [searchText, setSearchText] = useState("");
+  const [selectedTransaction, setSelectedTransaction] =
+    useState<Transaction | null>(null);
 
   const getMonth = (date: string) => {
     const $d = new Date(date);
-  
-    return new Intl.DateTimeFormat('pt-BR', { month: 'long' }).format($d);
-  }
+    return new Intl.DateTimeFormat("pt-BR", { month: "long" }).format($d);
+  };
 
   const getCompleteDate = (date: string) => {
     const $d = new Date(date);
-  
-    const { hour, minute, day, month, year} = formatDate($d);
-
-    return `${hour}h${String(minute).padStart(2, '0')} - ${day}/${month}/${year}`;
+    const { hour, minute, day, month, year } = formatDate($d);
+    return `${hour}h${String(minute).padStart(
+      2,
+      "0"
+    )} - ${day}/${month}/${year}`;
   };
 
   const getTransactionsList = async () => {
@@ -40,14 +42,23 @@ export default function Transactions() {
       const data = await getTransactions();
       setTransactions(data);
     } catch (err) {
-      console.log('errorGettingTransactions');
+      console.log("errorGettingTransactions");
     } finally {
       setLoading(false);
     }
-  }
+  };
 
-  const handleEditClick = (transaction) => {
-    setSelectedTransaction(transaction); // Define a transação selecionada
+  const filteredTransactions = transactions.filter(
+    (transaction) =>
+      (selectedFilter === "all" || transaction.type === selectedFilter) &&
+      (transaction.description
+        .toLowerCase()
+        .includes(searchText.toLowerCase()) || // Busca no campo descrição
+        String(transaction.amount).includes(searchText)) // Busca no campo valor
+  );
+
+  const handleEditClick = (transaction: Transaction) => {
+    setSelectedTransaction(transaction);
   };
 
   useEffect(() => {
@@ -60,12 +71,17 @@ export default function Transactions() {
         Extrato da conta
         <i className="ms-2 fa-solid fa-cash-register"></i>
       </h4>
-      
-      <div style={{ gap: 8, cursor: 'pointer' }} className="d-flex flex-wrap py-3">
-        {TRANSACTION_TYPES && Object.entries(TRANSACTION_TYPES).map(([key, value]) => (
-          <span 
-            key={key} 
-            className={`badge rounded-pill bg-${key === selectedFilter ? 'success' : 'secondary'}`} 
+
+      <div
+        style={{ gap: 8, cursor: "pointer" }}
+        className="d-flex flex-wrap py-3"
+      >
+        {Object.entries(TRANSACTION_TYPES).map(([key, value]) => (
+          <span
+            key={key}
+            className={`badge rounded-pill bg-${
+              key === selectedFilter ? "success" : "secondary"
+            }`}
             onClick={() => setSelectedFilter(key)}
           >
             {value}
@@ -74,18 +90,19 @@ export default function Transactions() {
       </div>
 
       <div className="input-group mb-3">
-        <input 
+        <input
           type="text"
           className="form-control"
-          placeholder="Buscar uma transação" 
+          placeholder="Buscar uma transação"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
         ></input>
         <span className="input-group-text" id="basic-addon2">
           <i className="fa-solid fa-magnifying-glass"></i>
         </span>
       </div>
-      
-      {(
-        loading &&
+
+      {loading && (
         <span
           className="spinner-border spinner-border-sm text-center mt-3 text-success d-block mx-auto"
           role="status"
@@ -93,16 +110,20 @@ export default function Transactions() {
         ></span>
       )}
 
-      {(
-        (!loading && !transactions.length) &&
-        <small className="d-block py-3 text-muted">Você ainda não possui transações</small>
+      {!loading && filteredTransactions.length === 0 && (
+        <small className="d-block py-3 text-muted">
+          Nenhuma transação encontrada.
+        </small>
       )}
 
-      {(
-        (!loading && transactions?.length) &&
-        transactions.map((transaction: Transaction, index) => (
+      {!loading &&
+        filteredTransactions.map((transaction: Transaction, index) => (
           <section className="list-group mt-3" key={index}>
-            <div style={{ cursor: 'pointer' }} className="list-group-item py-3" aria-current="true">
+            <div
+              style={{ cursor: "pointer" }}
+              className="list-group-item py-3"
+              aria-current="true"
+            >
               <div className="d-flex justify-content-between align-items-center mb-3">
                 <small>
                   <strong className="text-success text-capitalize">
@@ -111,8 +132,11 @@ export default function Transactions() {
                   </strong>
                 </small>
                 <div style={{ gap: 8 }} className="d-flex align-items-center">
-                  <span className="badge rounded-pill bg-secondary">{TRANSACTION_TYPES[transaction.type]}</span>
-                  <i className="fa-solid fa-edit text-success"
+                  <span className="badge rounded-pill bg-secondary">
+                    {TRANSACTION_TYPES[transaction.type]}
+                  </span>
+                  <i
+                    className="fa-solid fa-edit text-success"
                     data-bs-toggle="modal"
                     data-bs-target="#transactionModal"
                     onClick={() => handleEditClick(transaction)}
@@ -120,15 +144,18 @@ export default function Transactions() {
                 </div>
               </div>
               <div className="d-flex flex-column flex-sm-row justify-content-between align-items-sm-center">
-                <small className="text-capitalize">{transaction.description}</small>
-                <small className="text-muted">{ getCompleteDate(transaction.date) }</small>
+                <small className="text-capitalize">
+                  {transaction.description}
+                </small>
+                <small className="text-muted">
+                  {getCompleteDate(transaction.date)}
+                </small>
               </div>
             </div>
           </section>
-        ))
-      )}
+        ))}
 
       <TransactionModal transaction={selectedTransaction} />
     </>
-  )
+  );
 }
